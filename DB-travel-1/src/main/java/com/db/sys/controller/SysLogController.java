4a9e87a7529762006888bffc1c2a8a3ce91bfcf8
yq <yuequanhuabai@qq.com>
2020-07-27 17:14:21 +0800

    first commit project recordsgit status!

--- DB-travel-1/src/main/java/com/db/sys/controller/SysLogController.java
+++ DB-travel-1/src/main/java/com/db/sys/controller/SysLogController.java
@@ -3,6 +3,7 @@ package com.db.sys.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.db.sys.po.SysLog;
 import com.db.sys.server.SysLogService;
@@ -15,6 +16,8 @@ public class SysLogController {
 	@Autowired
 	private SysLogService sysLogService;
 
+	@RequestMapping("doFindPageObjects")
+	@ResponseBody
 	public JsonResult doFindPageObjects(String username,Integer pageCurrent) {
 		PageObject<SysLog> pageObjects = sysLogService.findObjectByPage(username, pageCurrent);
 		
